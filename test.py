4f5039fd36084ddc0d9c1fa1aa793006f22ac4bd
JIANG mingda <jiangrivers9511@gmail.com>
2019-12-13 00:32:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,20 @@ A Python3 document for testing leetcode
 
 @author: Jiang Rivers
 """
+
+def lengthOfLongestSubstring(s):
+    start=1
+    end=1
+    ans=0
+    map={}
+    for i in range(len(s)):
+        if s[i] not in map:
+            map[s[i]]=start
+            ans=max(end-start+1,ans)
+            end+=1
+        else:
+            start
+
+
+
+
