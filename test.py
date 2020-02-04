fcaeaac020481becb30875714736bab2759c7c8f
JiangRIVERS <jiangrivers9511@gmail.com>
2020-02-04 14:33:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,4 @@
-"""
-Filename:test.py
-A Python3 document for testing leetcode
-
-@author: Jiang Rivers
-"""
-
-def lengthOfLongestSubstring(s):
-    start=1
-    end=1
-    ans=0
-    map={}
-    for i in range(len(s)):
-        if s[i] not in map:
-            map[s[i]]=start
-            ans=max(end-start+1,ans)
-            end+=1
-        else:
-            start
-
-
-
-
+import torchvision
+import torch
+model=torchvision.models.resnet18()
+print(model)
